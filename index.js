1349af516629bb858579598098e5398c35d138d9
Oluwakayode Adesina <adesinak4@gmail.com>
2022-09-28 13:49:09 +0100

    update

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 require('dotenv').config();
+require('./controller/database');
 
 const express = require('express');
-const mongoose = require('mongoose');
-const mongoString = process.env.DATABASE_URL;
 const routes = require('./routes/routes');
 const route = require('./routes/index');
 
 const app = express();
-
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('Database Connected');
-})
 
 
 app.use(express.json());
